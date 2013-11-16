a26ea2a270b7a59f783bc300769585438ae75637
alexandre kirillov <alexandre.kirillov@gmail.com>
2013-11-16 18:48:37 +0900

    update demo: show matched text gray when it was matched

--- demo/js/index.js
+++ demo/js/index.js
@@ -15,16 +15,47 @@ $(function(){
         return getUniqueArray( tmpArray );
     };
 
+    var getUniqueElementListFromSource = function( $target ){
+        var tmpObj = {};
+        $target.each(function(){
+            if (! tmpObj.hasOwnProperty($(this).text())) {
+                tmpObj[ $(this).text() ] = [];
+            }
+            tmpObj[ $(this).text() ].push( $(this) );
+        });
+        return tmpObj;
+    };
+
     $(function(){
 
         $('.translation').each(function(){
             var $this = $(this),
                 $source = $this.find('.source'),
-                $target = $this.find('.target');
+                $target = $this.find('.target'),
+                elementObj = getUniqueElementListFromSource($this.find('.source').find('.match, .hoge'));
 
             $target
                 .autosize()
                 .on('textarea.highlighter.match', function(e, data){
+
+                    $source.find('.added').removeClass('added');
+
+                    var i = 0, j = 0, imax = 0, jmax = 0, tmpList = null, tmpText = null;
+
+                    imax = data.textList.length;
+
+                    for (i = 0; i < imax; i++) {
+                        tmpText = data.textList[i];
+
+                        if (elementObj[ tmpText ]) {
+                            tmpList = elementObj[ tmpText ];
+                            jmax = tmpList.length;
+
+                            for (j = 0; j < jmax; j++) {
+                                tmpList[j].addClass('added');
+                            }
+                        }
+                    }
                 })
                 .textareaHighlighter({
                     // maxlength: 150,
