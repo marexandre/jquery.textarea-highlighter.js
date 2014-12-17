8c65718a1e73f4097531a512e13bbfa35fda6d16
Alexandre KIRILLOV <alexandre.kirillov@gmail.com>
2014-12-17 16:56:24 +0900

    fix karma test failing with jquery

--- karma.conf.js
+++ karma.conf.js
@@ -2,16 +2,16 @@
 module.exports = function(config) {
   config.set({
     browsers: ['PhantomJS'],
-    frameworks: ['jasmine-jquery', 'jasmine'],
+    frameworks: ['jasmine'],
     files: [
       'bower_components/jquery/dist/jquery.min.js',
+      'bower_components/jasmine-jquery/lib/jasmine-jquery.js',
       'src/**/*.js',
       'spec/**/*.js'
     ],
     reporters: ['spec'],
     plugins: [
       'karma-jasmine',
-      'karma-jasmine-jquery',
       'karma-chrome-launcher',
       'karma-phantomjs-launcher',
       'karma-spec-reporter'
